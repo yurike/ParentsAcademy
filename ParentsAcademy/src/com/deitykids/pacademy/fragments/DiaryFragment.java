8434f575c9736e9be9eade08116076f272b2c834
artem pecheny <pecheny@gmail.com>
2014-05-03 14:52:16 +0400

    Show records in the diary

--- ParentsAcademy/src/com/deitykids/pacademy/fragments/DiaryFragment.java
+++ ParentsAcademy/src/com/deitykids/pacademy/fragments/DiaryFragment.java
@@ -1,7 +1,5 @@
 package com.deitykids.pacademy.fragments;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -10,49 +8,40 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioGroup;
 import android.widget.TextView;
+import com.deitykids.pacademy.Main;
 import com.deitykids.pacademy.R;
+import org.joda.time.LocalDate;
 
 /**
- *
  * Created by Yuri K on 02.05.2014.
  */
-public class DiaryFragment extends Fragment
-{
+public class DiaryFragment extends Fragment {
 
     private Button button;
     private EditText editText;
+    private TextView textView;
+    private LocalDate now;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View contentView = inflater.inflate(R.layout.diary, container, false);
 
         editText = ((EditText) contentView.findViewById(R.id.editText));
         button = (Button) contentView.findViewById(R.id.button);
+        textView = (TextView) contentView.findViewById(R.id.textView2);
         final FragmentActivity activity = this.getActivity();
+        final int age = Main.getInstance().getYears();
+        now = new LocalDate();
 
         button.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        new AlertDialog.Builder(activity)
-                                .setTitle("")
-                                .setMessage("Ваше сообщение отправлено")
-                                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        // continue with delete
-                                        editText.setText("");
-                                    }
-                                })
-//                                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-//                                    public void onClick(DialogInterface dialog, int which) {
-//                                        // do nothing
-//                                    }
-//                                })
-                                .setIcon(android.R.drawable.ic_dialog_alert)
-                                .show();
+                        textView.append("\n\n\n");
+                        textView.append(now.toString() + ", возраст " + age + ": ");
+                        textView.append(editText.getText());
+                        editText.setText("");
                     }
                 }
         );
